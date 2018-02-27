eff510dd3394d2db2bf90943754fefd1c858acfa
srighin <sriman456.singh@gmail.com>
2018-02-28 00:00:10 +0800

    Adding code for eureka and config service

--- src/main/java/com/sriman/eventmanagement/eventmanagementapi/EventmanagementApiApplication.java
+++ src/main/java/com/sriman/eventmanagement/eventmanagementapi/EventmanagementApiApplication.java
@@ -1,11 +1,16 @@
 package com.sriman.eventmanagement.eventmanagementapi;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.data.convert.Jsr310Converters;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@EnableDiscoveryClient
 @EntityScan(basePackageClasses = {EventmanagementApiApplication.class, Jsr310Converters.class})
 public class EventmanagementApiApplication {
 
